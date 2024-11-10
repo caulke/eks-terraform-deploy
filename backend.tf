b84535e83949278a7f3afa2498ebc78d5bf7fe56
caulke <kelvin.caulker.tripticom@gmail.com>
2024-11-10 22:03:06 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "kevin/terraform.state"
-    bucket         = "fabiola-terraform-backend-bucket"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-state-locking"
+    bucket         = "team1-bucket-001"
+    region         = "us-east-1"
+    dynamodb_table = "team1-db-001"
   }
 }
